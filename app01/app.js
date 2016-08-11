a95caa28f564ed7824ad9073158a021afbb1fa69
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2016-08-11 20:36:56 +0100

    corrected db setup errors wrt const declaration of db variable

--- app01/app.js
+++ app01/app.js
@@ -8,6 +8,7 @@ const
   bParser = require('body-parser'),
   compression = require('compression'),
   path = require('path'),
+  config = require('./config/config'),
   mongoose = require('mongoose');
 //=============================================================================
 /**
@@ -20,7 +21,12 @@ const app = express();
  * Module variables
  */
 //=============================================================================
-const port = process.env.port || 3030;
+const
+  port = process.env.port || 3030,
+  env = config.env,
+  host = config.host,
+  dBURL = config.dBURL;
+var db;
 //=============================================================================
 /**
  * App config and settings
@@ -29,6 +35,31 @@ const port = process.env.port || 3030;
 require('clarify')
 app.disable('x-powered-by');
 app.set('port', port);
+app.set('env', env);
+app.set('host', host);
+//=============================================================================
+/**
+ * dBase connection
+ */
+//=============================================================================
+mongoose.connect(dBURL);
+db = mongoose.connection;
+db.on('error', function (err) {
+  console.error('There was a db connection error');
+  return  console.error(err.message);
+});
+db.once('connected', function () {
+  return console.log('Successfully connected to ' + dBURL);
+});
+db.once('disconnected', function () {
+  return console.error('Successfully disconnected from ' + dBURL);
+});
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    console.error('dBase connection closed due to app termination');
+    return process.exit(0);
+  });
+});
 //=============================================================================
 /**
  * Middleware Stack
