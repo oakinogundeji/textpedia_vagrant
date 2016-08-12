0445ebc9b16728ea3a4e66d38b355f942adfff9b
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2016-08-12 17:39:18 +0100

    fixed issues with variations in wikipedia formatting

--- app01/routes/routes.js
+++ app01/routes/routes.js
@@ -35,11 +35,35 @@ router.post('/scrape', function (req, res) {
   console.log('scrape user phone number %s', req.body.p_num);
   console.log('scrape keyword', req.body.k_words);
   //NB child_process pattern is 'cmd, [file_path, args.....]'
+  /*var words = req.body.k_words.split(',');
+  for(var i = 0; i < words.length; i++) {
+    console.log('k_word', words[i]);
+    console.log('calling scraper');
+    var
+      args = [SCRAPER_PATH, words[i]],
+      cp = require('child_process'),
+      scraper = cp.spawn('python', args),
+      chunk = '';
+
+    scraper.stdout.on('data', function (data) {
+      console.log('received data from scraper');
+      chunk += data
+    });
+    scraper.stdout.on('close', function () {
+      console.log('scraper finished sending data');
+      console.log('data =', chunk);
+      //return res.status(200).json(chunk)
+    });
+    scraper.stderr.on('data', function (err) {
+      console.log('there was an err with scraper');
+      return console.error(err.toString());
+    });
+  }*/
   var
     words = req.body.k_words.split(','),
     args = [SCRAPER_PATH].concat(words),
     cp = require('child_process'),
-    scraper = cp.spawn('python', args),
+    scraper = cp.spawn('python3', args),
     chunk = '';
 
   scraper.stdout.on('data', function (data) {
@@ -48,13 +72,13 @@ router.post('/scrape', function (req, res) {
   scraper.stdout.on('close', function () {
     console.log('scraper finished sending data');
     console.log('data =', chunk);
-    return res.status(200).json(chunk)
+    //return res.status(200).json(chunk)
   });
   scraper.stderr.on('data', function (err) {
     console.log('there was an err with scraper');
     return console.error(err.toString());
   });
-  //return res.status(200).json('Got it!');
+  return res.status(200).json('Got it!');
 });
 //=============================================================================
 /**
--- app01/scraper/scraper.py
+++ app01/scraper/scraper.py
@@ -1,48 +1,65 @@
 import argparse
 import sys
+import re
 import requests as req
 from bs4 import BeautifulSoup as BSoup
 
 
 def scrape(k_words):
-    #print 'received k_words from Main', k_words
-    #print 'number of k_words', len(k_words)
-    #for word in k_words:
-    #    print 'k_word =', word
-    #return len(k_words)
+    print('input =', k_words)
     baseURL = 'https://en.wikipedia.org/wiki/'
     targetURL = baseURL + k_words[0]
-    print 'targetURL', targetURL
+    regex = '^\[\d\]$'
+    #print('targetURL', targetURL)
     try:
-        print 'attempting to hit wikipedia'
+        #print('attempting to hit wikipedia')
         html = req.get(targetURL)
     except ConnectionError as err:
-        print 'Error making network connection'
-        print err
+        print('Error making network connection')
+        print(err)
     except:
-        print 'Network connection error'
+        print('Network connection error')
     else:
-        print 'successfully retrieved data from wikipedia'
+        #print('successfully retrieved data from wikipedia')
         bsObj = BSoup(html.text, 'html.parser')
-        toc = bsObj.find('div', id='toc')
-        p_list = toc.find_previous_siblings('p')
-        p_list.reverse()
-        print 'num paras = ', len(p_list)
-        for para in p_list:
-            if len(para) == 0:
-                continue
-            else:
-                print 'para content:'
-                print 'para length:', len(para)
-                print para.get_text(), '\n'
+        try:
+            toc = bsObj.find('div', id='toc')
+            p_list = toc.find_previous_siblings('p')
+        except AttributeError:
+            div = bsObj.find('div', id='mw-content-text')
+            h2 = div.find('h2')
+            #print('h2', h2)
+            p_list = h2.find_previous_siblings('p')
+            p_list.reverse()
+            #print('num paras = ', len(p_list))
+            for para in p_list:
+                if len(para) == 0:
+                    continue
+                else:
+                    #print('para content:')
+                    #print('para length:', len(para))
+                    nstr = re.sub(r'\[\d\]', '', para.get_text())
+                    print(nstr, '\n')
+        else:
+            p_list.reverse()
+            #print('num paras = ', len(p_list))
+            for para in p_list:
+                if len(para) == 0:
+                    continue
+                else:
+                    #print('para content:')
+                    #print('para length:', len(para))
+                    nstr = re.sub(r'\[\d\]', '', para.get_text())
+                    print(nstr, '\n')
 
 
 def Main():
     parser = argparse.ArgumentParser(description="Webscraping Module")
     parser.add_argument('k_words', type=str, nargs='+')
+    #parser.add_argument('k_words', type=str)
     args = parser.parse_args()
     result = scrape(args.k_words)
-    print 'result =', result
+    #print('result =', result)
     #sys.stdout.flush()
 
 
